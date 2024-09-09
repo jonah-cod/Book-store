46fb59b7d7e5320135f494d11c1874a17b048e4c
Jonathan Mwaniki <idolearndaily@gmail.com>
2024-09-09 12:36:38 +0300

    book store implementation

---
+++ data.js
@@ -0,0 +1,94 @@
+const bookStore = [
+    {
+      id: 1,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      genre: "Fiction"
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+      genre: "Fiction"
+    },
+    {
+      id: 3,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      genre: "Fiction"
+    },
+    {
+      id: 4,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      genre: "Fiction"
+    },
+    {
+      id: 5,
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      genre: "Fiction"
+    },
+    {
+      id: 6,
+      title: "Sapiens: A Brief History of Humankind",
+      author: "Yuval Noah Harari",
+      genre: "Non-Fiction"
+    },
+    {
+      id: 7,
+      title: "Educated",
+      author: "Tara Westover",
+      genre: "Non-Fiction"
+    },
+    {
+      id: 8,
+      title: "The Immortal Life of Henrietta Lacks",
+      author: "Rebecca Skloot",
+      genre: "Non-Fiction"
+    },
+    {
+      id: 9,
+      title: "Becoming",
+      author: "Michelle Obama",
+      genre: "Non-Fiction"
+    },
+    {
+      id: 10,
+      title: "Quiet: The Power of Introverts in a World That Can’t Stop Talking",
+      author: "Susan Cain",
+      genre: "Non-Fiction"
+    },
+    {
+      id: 11,
+      title: "Dune",
+      author: "Frank Herbert",
+      genre: "Science Fiction & Fantasy"
+    },
+    {
+      id: 12,
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      genre: "Science Fiction & Fantasy"
+    },
+    {
+      id: 13,
+      title: "Foundation",
+      author: "Isaac Asimov",
+      genre: "Science Fiction & Fantasy"
+    },
+    {
+      id: 14,
+      title: "The Name of the Wind",
+      author: "Patrick Rothfuss",
+      genre: "Science Fiction & Fantasy"
+    },
+    {
+      id: 15,
+      title: "Neuromancer",
+      author: "William Gibson",
+      genre: "Science Fiction & Fantasy"
+    }
+  ]
+
+  module.exports  = bookStore
